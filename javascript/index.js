f106af914e2628bfee8c79d90caaf38f871439c1
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2022-01-16 11:10:50 -0300

    funcao getActions

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,18 @@
 const $teclas = document.querySelector('#teclas')
+const $actions = document.querySelector('#actions')
 const $visor = document.querySelector('.visor')
 
 function insertTeclas(e) {
     const tecla = e.target.id
-    console.log(tecla)
-
     $visor.innerHTML += tecla
+
     console.log($visor)
 }
 
+function getAction(e) {
+    console.log('[getAction]: ' ,e.target)
+}
+
 $teclas.addEventListener( 'click', insertTeclas )
+
+$actions.addEventListener( 'click', getAction )
